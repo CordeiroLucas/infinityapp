989bd90a8835eb8e21caeff207bafccb7daa41eb
kaikinattandossantos <kaikinattansg@gmail.com>
2025-09-24 19:17:58 -0300

    refactor project

---
+++ login/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+from django.contrib import messages
+
+# Create your views here.
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            auth_login(request, user)
+            messages.success(request, f'Bem-vindo, {username}')
+            return HttpResponse("Logado com sucesso")
+        else:
+            messages.error(request, 'Credenciais inválidas.')
+            return redirect('login')
+        
+    return render(request, 'login.html')
