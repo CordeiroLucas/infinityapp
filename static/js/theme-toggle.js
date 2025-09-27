cf211c959ad355d2fa52992e8c4bc3f50d3bf99c
CordeiroLucas <pacheco.lucascordeiro@gmail.com>
2025-09-27 11:51:49 -0300

    fix: mensagens de notificacao

--- static/js/theme-toggle.js
+++ static/js/theme-toggle.js
@@ -1,13 +1,16 @@
+const DARK_MODE = "Modo Escuro";
+const LIGHT_MODE = "Modo Claro";
+
 const theme = localStorage.getItem("theme") || "dark-mode";
 document.documentElement.setAttribute("theme", theme);
 
 const themeToggleBtn = document.getElementById("toggle-theme");
 
 if (themeToggleBtn.classList.contains("btn-light") && theme === "light-mode") {
-  themeToggleBtn.textContent = "Dark";
+  themeToggleBtn.textContent = DARK_MODE;
   themeToggleBtn.classList.replace("btn-light", "btn-dark");
 } else {
-  themeToggleBtn.textContent = "Light";
+  themeToggleBtn.textContent = LIGHT_MODE;
   themeToggleBtn.classList.replace("btn-dark", "btn-light");
 }
 
@@ -15,7 +18,8 @@ themeToggleBtn.addEventListener("click", () => {
   const currentTheme = document.documentElement.getAttribute("theme");
   const newTheme = currentTheme === "light-mode" ? "dark-mode" : "light-mode";
 
-  themeToggleBtn.textContent = newTheme === "light-mode" ? "Dark" : "Light";
+  themeToggleBtn.textContent =
+    newTheme === "light-mode" ? DARK_MODE : LIGHT_MODE;
 
   document.documentElement.setAttribute("theme", newTheme);
   localStorage.setItem("theme", newTheme);
