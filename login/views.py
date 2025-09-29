8bb412b372759bf087979b19ac09510b78385aff
cordeirolucas <pacheco.lucascordeiro@gmail.com>
2025-09-29 17:33:34 -0300

    feat: register view

--- login/views.py
+++ login/views.py
@@ -21,17 +21,33 @@ def login_view(request):
                 return redirect("dashboard")
             else:
                 messages.error(request, 'Credenciais inválidas.')
-                saida = {'notificacao':{'tipo':'danger', 'message':'Credenciais Inválidas'}}
-                return render(request, 'login/login.html', saida)
+                out = {'popup':{'type':'danger', 'message':'Credenciais Inválidas'}}
+                return render(request, 'login/login.html', out)
         except OperationalError:
                 messages.error(request, 'Servidor Inativo.')
-                saida = {'notificacao':{'tipo':'danger', 'message':'Server Inativo'}}
-                return render(request, 'login/login.html', saida)
+                out = {'popup':{'type':'danger', 'message':'Server Inativo'}}
+                return render(request, 'login/login.html', out)
         
     return render(request, 'login/login.html')
+
+def register_view(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        pass_confirmation = request.POST['password_check']
+        
+        if password != pass_confirmation:
+            out = {'popup':{'type':'danger', 'message':'Insira senhas iguais!'}}
+            return render(request, 'login/register.html', out)
+        
+        out = {'popup':{'type':'success', 'message':'Registrado com sucesso'}}
+        return render(request, 'login/register.html', out)
+        
+        
+    return render(request, 'login/register.html')
 
 @login_required
 def logout_view(request):
     logout(request)
-    saida = {'notificacao':{'tipo':'success', 'message':'Desconectado com sucesso'}}
-    return render(request, 'login/login.html', saida)
+    out = {'popup':{'type':'success', 'message':'Desconectado com sucesso'}}
+    return render(request, 'login/login.html', out)
